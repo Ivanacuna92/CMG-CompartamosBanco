b3765719144c79659e488303642d70e9e8ab3fa5
Erick Hernandez <eohh20011@gmail.com>
2025-05-13 09:43:20 -0600

    Cambio orden de listado en los reportes

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -27,7 +27,7 @@ export async function listConversations(req, res) {
     FROM esac_conversations
     WHERE (? = '' OR MONTH(last_update) = ?)
       AND (? = '' OR DAY(last_update) = ?)
-    ORDER BY last_update DESC
+    ORDER BY id ASC
     LIMIT ? OFFSET ?
   `, [month, month, day, day, Number(size), offset]);
   const [[{ total }]] = await pool.query("SELECT FOUND_ROWS() AS total");
