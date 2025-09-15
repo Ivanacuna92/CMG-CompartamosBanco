3ead127932ebbe7ac34f25fc51c525590d2cd679
UliVeraC <veracuevasu@gmail.com>
2025-09-15 15:31:51 -0700

    Revert "Feature: Mostrar cuenta"
    This reverts commit f3270f718aa211e236d502fee9e53fb709bfa27c.

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -23,11 +23,7 @@ export async function listConversations(req, res) {
   const offset = (page - 1) * size;
   const [data] = await pool.query(`
     SELECT SQL_CALC_FOUND_ROWS
-      id, uuid, last_update, payment_agreement, estimated_recovery, total_interactions, contract_number,
-      CASE
-        WHEN payment_agreement = 1 THEN contract_number
-        ELSE '-'
-      END AS cuenta
+      id, uuid, last_update, payment_agreement, estimated_recovery, total_interactions, contract_number
     FROM esac_conversations
     WHERE (? = '' OR MONTH(last_update) = ?)
       AND (? = '' OR DAY(last_update) = ?)
